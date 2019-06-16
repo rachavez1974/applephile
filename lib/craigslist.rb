abb4bcc3768d89dc498fbf48c17dc8d1d3568336
Ramon Chavez <rachavez1974@gmail.com>
2019-06-16 18:16:55 -0400

    Add #get_states_names and make test pass

--- lib/craigslist.rb
+++ lib/craigslist.rb
@@ -10,5 +10,10 @@ class CraigsList
     @doc = Nokogiri::HTML(open(@site_url))  
   end
 
+  def get_states_names
+    #it returns an array of states of the U.S., CLI will use to display them
+    doc.css(".colmask").first.css("h4").collect { |st| st.text}  
+  end
+
 
 end
