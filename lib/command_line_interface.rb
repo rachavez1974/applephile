c6b3d57ccc99ffac6a42e08a052597c997a3399f
Ramon Chavez <rachavez1974@gmail.com>
2019-06-18 03:02:53 -0400

    #isplay_main_menu

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,6 +1,16 @@
 class CLI
+  INTRO_MESSAGE = "WELCOME TO CRAIGSLIST SCRAPPER!"
+
   def run
     
+  
+  def display_main_menu
+    stars = "************************".colorize(:yellow)
+    puts "#{stars} MAIN MENU #{stars}" 
+    puts "Enter \"scrape\" to scrape.".colorize(:blue)
+    puts "Enter \"exit\" to end program.".colorize(:yellow)
+    puts "What would you like to do?".colorize(:blue)
+    gets.chomp
   end
 
   def get_choice
