9aef6658087959fde5e930a08b2f0788ee1a1e38
Ramon Chavez <rachavez1974@gmail.com>
2019-06-18 03:14:13 -0400

    Add #convert_to_state

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -50,6 +50,11 @@ class CLI
     print "\n"
   end
 
+  def convert_to_state(state_number)
+    #it takes in a number and returns the corresponding state's name
+    @scrape.get_states_names[state_number.to_i - 1]
+  end
+
 
 
   def get_choice
