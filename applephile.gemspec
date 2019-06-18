be64e7f3d477d56a13895f53df52373a60b836d9
Ramon Chavez <rachavez1974@gmail.com>
2019-06-18 00:56:32 -0400

    Remove applephile-0.1.0.gem to recompile gem again

--- applephile.gemspec
+++ applephile.gemspec
@@ -3,6 +3,7 @@ lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require "applephile/version"
 
+
 Gem::Specification.new do |spec|
   spec.name          = "applephile"
   spec.version       = Applephile::VERSION
--- lib/applephile.rb
+++ lib/applephile.rb
@@ -1,6 +1,13 @@
 require_relative "../lib/applephile/version"
+require_relative '../config/environment.rb'
+# require_relative '../lib/applephile/craigslist'
 
-module Applephile
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module Applephile
+#   class Error < StandardError; end
+#   class Hello
+#     def hi
+#       puts "hello mundo!"
+#     end
+#   end
+#   # Your code goes here...
+# end
