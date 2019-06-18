5eaf5ebd9ce71ed1bd9cc21dd9aeb2f59ca06fc4
Ramon Chavez <rachavez1974@gmail.com>
2019-06-18 03:06:44 -0400

    Add #display_states

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,6 +2,9 @@ class CLI
   INTRO_MESSAGE = "WELCOME TO CRAIGSLIST SCRAPPER!"
 
   def run
+    @scrape = CraigsList.
+
+  end
     
   
   def display_main_menu
@@ -12,6 +15,18 @@ class CLI
     puts "What would you like to do?".colorize(:blue)
     gets.chomp
   end
+
+  def display_states
+    @scrape.get_states_names.each_with_index do |state, index|
+      print "#{index + 1}. #{state}    ".ljust(28) 
+      if (index + 1) % 5 == 0
+        print "\n"
+      end
+    end
+    print "\n"
+  end 
+
+
 
   def get_choice
     puts "Enter number to see link phone on browser.".colorize(:cyan)
