065e48a560769f55778ddc743420f6f76a916d59
Ramon Chavez <rachavez1974@gmail.com>
2019-06-18 00:59:56 -0400

    Finally set up gem so no need for namespce calling when instatiating class objects

--- lib/applephile.rb
+++ lib/applephile.rb
@@ -1,13 +1,3 @@
 require_relative "../lib/applephile/version"
 require_relative '../config/environment.rb'
-# require_relative '../lib/applephile/craigslist'
 
-# module Applephile
-#   class Error < StandardError; end
-#   class Hello
-#     def hi
-#       puts "hello mundo!"
-#     end
-#   end
-#   # Your code goes here...
-# end
