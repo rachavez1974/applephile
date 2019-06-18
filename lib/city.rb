c20acea51d6a11ce519b1be31b6b593473e5a6d7
Ramon Chavez <rachavez1974@gmail.com>
2019-06-18 02:54:48 -0400

    Add #get_apple_prods_by_price and pass test

--- lib/city.rb
+++ lib/city.rb
@@ -16,16 +16,7 @@ class City
     item
   end
 
-  describe "#get_apple_prods_by_price" do
-    it "it accpets a price, then it returns all
-      the items that are greater than or equals to that price" do
-      city_a_hash = {:name => "las vegas", :state => "Nevada",
-                      :city_url => "https://lasvegas.craigslist.org/"}
-      city_a = City.new(city_a_hash)
-      city_a = add_items_to_city(city_a)
-      length = city_a.get_apple_prods_by_price(200).length
-      expect(city_a.get_apple_prods_by_price(200)[rand(0..length - 1)].price).to be >= "200"
-      
-    end
+  def get_apple_prods_by_price(set_price)
+    @items.select { |item| item.price.to_i >= set_price.to_i }
   end
 end
